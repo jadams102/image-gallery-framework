20a17624d14e5cd6aec462be7528480987707f97
Joel Adams <joeladamsdesign@gmail.com>
2019-07-31 15:29:03 -0700

    add upload service from previous project

---
+++ src/app/services/upload.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Upload } from '../models/upload.model';
+import * as  firebase from 'firebase';
+
+@Injectable()
+export class UploadService {
+
+  public basePath = '/galleries';
+
+  constructor(private db: AngularFireDatabase ) { }
+
+  uploadFiles(upload: Upload) {
+    const storageRef = firebase.storage().ref();
+    const uploadTask = storageRef.child(`${this.basePath}/${upload.file.name}`).put(upload.file);
+
+    uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+    (snapshot) => {
+      upload.progress = (uploadTask.snapshot.bytesTransferred / uploadTask.snapshot.totalBytes) * 100;
+      console.log(upload.progress);
+    },
+
+    (error) => {
+      console.log(error);
+    },
+
+    ():any => {
+      upload.url = uploadTask.snapshot.downloadURL;
+      upload.title = upload.file.name;
+      this.saveFileData(upload);
+    }
+  );
+}
+
+deleteFile(name) {
+  const storageRef = firebase.storage().ref();
+  const imgRef = storageRef.child( '/galleries/' + name);
+  imgRef.delete()
+}
+
+private saveFileData(upload: Upload) {
+    this.db.list(`${this.basePath}/`).push(upload);
+    console.log("Files Saved: " + upload.url)
+  }
+}
