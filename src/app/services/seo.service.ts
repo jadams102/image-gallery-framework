f739774d1663d6cae4391e811c79acff11f30201
Joel Adams <joeladamsdesign@gmail.com>
2019-08-01 14:55:09 -0700

    add seo service, update meta tags

--- src/app/services/seo.service.ts
+++ src/app/services/seo.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class SeoService {
 
-  constructor() { }
+  constructor(private meta: Meta) { }
+
+  generateTags(config) {
+    // default values
+    config = { 
+      title: 'Angular <3 Linkbots', 
+      description: 'My SEO friendly Angular Component', 
+      image: 'https://angularfirebase.com/images/logo.png',
+      slug: '',
+      ...config
+    }
+    this.meta.updateTag({ name: 'twitter:title', content: config.title });
+    this.meta.updateTag({ name: 'twitter:description', content: config.description });
+    this.meta.updateTag({ name: 'twitter:image', content: config.image });
+
+    this.meta.updateTag({ property: 'og:title', content: config.title });
+    this.meta.updateTag({ property: 'og:description', content: config.description });
+    this.meta.updateTag({ property: 'og:image', content: config.image });
+    this.meta.updateTag({ property: 'og:url', content: `https://instafire-app.firebaseapp.com/${config.slug}` });
+  }
+
 }
