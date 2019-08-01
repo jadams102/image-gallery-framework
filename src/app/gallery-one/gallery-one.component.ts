7e0f8054ecce992d75887a68dae415b986a79e19
Joel Adams <joeladamsdesign@gmail.com>
2019-08-01 13:37:25 -0700

    add test gallery logic

--- src/app/gallery-one/gallery-one.component.ts
+++ src/app/gallery-one/gallery-one.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Upload } from '../models/upload.model';
+import { AuthenticationService } from '../services/authentication.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-gallery-one',
@@ -7,9 +13,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryOneComponent implements OnInit {
 
-  constructor() { }
+  galleryName: string;
+  images: Upload[][];
+  keys: String[];
+  user: Observable<firebase.User>;
+
+  constructor(private authService: AuthenticationService, private imageService: ImageService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.galleryName = 'GalleryOne'
+    this.imageService.setGallery(this.galleryName.toLowerCase());
+    this.imageService.getGallery().valueChanges().subscribe((data) => {
+      this.imageService.getGallery().snapshotChanges().pipe(
+        map(actions => actions.map(a=> ({key: a.key}))
+        )).subscribe(keys => {
+          this.images = data;
+          this.keys = keys.map(item => item.key);
+          console.log(this.keys);
+        })
+    })
+
+    // this.imageService.getGallery().snapshotChanges().pipe(
+    //   map(actions => 
+    //     actions.map(a=> ({key: a.key, ...a.payload.val() }))
+    //   ))
+    //   .subscribe(data => {
+    //   this.images = data.map(item => item.key);
+    //   console.log(data);
+    // });
+
+    this.user = this.authService.authUser();
+  }
+
+  goToImageDetail(gallery: string, key: string) {
+    this.router.navigate([gallery, key]);
+  }
+
+  deleteImage(image: Upload, key) {
+    this.imageService.removeImage(image.gallery, key, image.title);
   }
 
 }
