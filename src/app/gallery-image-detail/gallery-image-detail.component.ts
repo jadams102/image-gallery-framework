323ad8b800a80a415c1aa13b227eab1b49b6b89c
Joel Adams <joeladamsdesign@gmail.com>
2019-08-01 13:26:02 -0700

    add logic to view indivdual gallery images

--- src/app/gallery-image-detail/gallery-image-detail.component.ts
+++ src/app/gallery-image-detail/gallery-image-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Upload } from '../models/upload.model';
+import { ImageService } from '../services/image.service';
+import { AuthenticationService } from '../services/authentication.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-gallery-image-detail',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryImageDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private imageService: ImageService, private route: ActivatedRoute, private router: Router,) { }
+
+  galleryName: string;
+  imageKey: string;
+  imageToDisplay: any;
+  user: Observable<firebase.User>
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.galleryName = urlParameters['name']
+      this.imageKey = urlParameters['id'];
+    });
+    this.imageService.getImageById(this.galleryName.toLowerCase(), this.imageKey).valueChanges().subscribe(
+      data => {
+        this.imageToDisplay = data;
+      }
+    )
+    this.user = this.authService.authUser();
   }
 
 }
