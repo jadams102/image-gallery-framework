10b82827ed6f5942d6308de599b39b5a5af848e3
Joel Adams <joeladamsdesign@gmail.com>
2019-08-04 15:23:42 -0700

    Update blog.component.ts

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../services/blog-post.service';
+import { Router } from '@angular/router';
+import { BlogPost } from '../models/blog-post.model';
+import { AuthenticationService } from '../services/authentication.service';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase/app';
 import { SeoService } from '../services/seo.service';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-blog',
@@ -7,11 +15,23 @@ import { SeoService } from '../services/seo.service';
   styleUrls: ['./blog.component.scss']
 })
 export class BlogComponent implements OnInit {
+  posts: BlogPost[];
+  user: Observable<firebase.User>
+  keys: String[];
 
-  constructor(private seo: SeoService) { }
+  constructor(private authService: AuthenticationService, private postService: BlogPostService, private router: Router, private seo: SeoService) { }
   
   ngOnInit() {
-    
+    this.postService.getPosts().valueChanges().subscribe(data => {
+      this.postService.getPosts().snapshotChanges().pipe(
+        map(actions => actions.map(a=> ({key: a.key}))
+        )).subscribe(keys => {
+          this.keys = keys.map(item => item.key).reverse();
+          this.posts = data.reverse();
+        })
+      })
+    this.user = this.authService.authUser();
+
     this.seo.generateTags({
       title: 'Blog Page', 
       description: 'This page has a blog on it', 
@@ -20,4 +40,14 @@ export class BlogComponent implements OnInit {
     })
   }
 
-}
+  goToDetailPage(clickedPost) {
+    this.router.navigate(['blog', clickedPost.$key]);
+  }
+
+  deletePost(post) {
+    this.postService.deletePost(post);
+  }
+    
+
+  }
+
