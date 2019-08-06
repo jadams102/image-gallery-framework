308ca30aa6b0eaa81a8578113354f3177e0ad2bf
Joel Adams <joeladamsdesign@gmail.com>
2019-08-05 18:30:52 -0700

    Update blog-add.component.ts

--- src/app/blog-add/blog-add.component.ts
+++ src/app/blog-add/blog-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BlogPostService } from '../services/blog-post.service';
+import { BlogPost } from '../models/blog-post.model';
 
 @Component({
   selector: 'app-blog-add',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-add.component.scss']
 })
 export class BlogAddComponent implements OnInit {
+  addingPost: boolean;
 
-  constructor() { }
+  constructor(private postService: BlogPostService) { }
 
   ngOnInit() {
+    this.addingPost = false;
+  }
+
+  toggleNewPost() {
+    if(!this.addingPost) {
+      this.addingPost = true;
+    } else {
+      this.addingPost = false;
+    }
+  }
+
+  submitPost(title: string, body: string) {
+    if (title === '') {
+      return alert('Please Enter a Title')
+    } else if (body === '') {
+      return alert('Please enter a post body')
+    } else {
+      this.addingPost = false;
+      const date = new Date().toDateString()
+      let newPost = new BlogPost(title, body, date)
+      this.postService.addPost(newPost);
+    }
+
   }
 
 }
