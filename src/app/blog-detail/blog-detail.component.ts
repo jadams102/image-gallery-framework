fd0852e598fb5d9e7d72c3853b9345c78265781b
Joel Adams <joeladamsdesign@gmail.com>
2019-08-06 16:25:00 -0700

    Update blog-detail.component.ts

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BlogPostService } from '../services/blog-post.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-blog-detail',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogDetailComponent implements OnInit {
 
-  constructor() { }
+  postId: string;
+  postToDisplay;
+
+  constructor(private route: ActivatedRoute, private location: Location, private postService: BlogPostService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.postId = urlParameters['id'];
+    });
+    this.postToDisplay = this.postService.getPostById(this.postId);
   }
 
 }
