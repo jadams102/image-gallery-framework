46ae06e8fb05079a7afff439e47b60fac6a7bb5d
Joel Adams <joeladamsdesign@gmail.com>
2019-08-20 15:46:08 -0700

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image.service';
+import { Upload } from '../models/upload.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  imageZero: Upload[];
+  imageOne: Upload[];
+  imageTwo: Upload[];
+
+
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
+    this.imageService.getGallery('galleryzero').valueChanges().subscribe(data => {
+      this.imageZero = data[0];
+    });
+
+    this.imageService.getGallery('galleryone').valueChanges().subscribe(data => {
+      this.imageOne = data[0];
+    });
+
+    this.imageService.getGallery('gallerytwo').valueChanges().subscribe(data => {
+      this.imageTwo = data[0];
+    });
   }
 
 }
